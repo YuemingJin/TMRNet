104d248b37287990d07a53dbacedf7182e31fea9
Yueming Jin <ymjin5341@gmail.com>
2021-03-30 21:31:39 +0800

    updated

---
+++ step2/NLBlock_MutiConv6.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+from torchsummary import summary
+from torch.nn import DataParallel
+import numpy as np
+
+
+class NLBlock(nn.Module):
+    def __init__(self, feature_num=512):
+        super(NLBlock, self).__init__()
+        self.linear1 = nn.Linear(feature_num, feature_num)
+        self.linear2 = nn.Linear(feature_num, feature_num)
+        self.linear3 = nn.Linear(feature_num, feature_num)
+        self.linear4 = nn.Linear(feature_num, feature_num)
+        self.layer_norm = nn.LayerNorm([1, 512])
+        self.dropout = nn.Dropout(0.2)
+
+        init.xavier_uniform_(self.linear1.weight)
+        init.xavier_uniform_(self.linear2.weight)
+        init.xavier_uniform_(self.linear3.weight)
+        init.xavier_uniform_(self.linear4.weight)
+
+    def forward(self, St, Lt):
+        St_1 = St.view(-1, 1, 512)
+        St_1 = self.linear1(St_1)
+        Lt_1 = self.linear2(Lt)
+        Lt_1 = Lt_1.transpose(1, 2)
+        SL = torch.matmul(St_1, Lt_1)       
+        SL = SL * ((1/512)**0.5)
+        SL = F.softmax(SL, dim=2)
+        Lt_2 = self.linear3(Lt)
+        SLL = torch.matmul(SL, Lt_2)
+        SLL = self.layer_norm(SLL)
+        SLL = F.relu(SLL)
+        SLL = self.linear4(SLL)
+        SLL = self.dropout(SLL)
+        SLL = SLL.view(-1, 512)
+        return (St+SLL)
+
+    
+class TimeConv(nn.Module):
+    def __init__(self):
+        super(TimeConv, self).__init__()
+        self.timeconv1 = nn.Conv1d(512, 512, kernel_size=3, padding=1)
+        self.timeconv2 = nn.Conv1d(512, 512, kernel_size=5, padding=2)
+        self.timeconv3 = nn.Conv1d(512, 512, kernel_size=7, padding=3)
+        self.maxpool = nn.AdaptiveMaxPool2d((512,1))
+
+    def forward(self, x):
+        x = x.transpose(1, 2)
+        
+        x1 = self.timeconv1(x)
+        y1 = x1.transpose(1, 2)
+        y1 = y1.view(-1,30,512,1)
+
+        x2 = self.timeconv2(x)
+        y2 = x2.transpose(1, 2)
+        y2 = y2.view(-1,30,512,1)
+
+        x3 = self.timeconv3(x)
+        y3 = x3.transpose(1, 2)
+        y3 = y3.view(-1,30,512,1)
+
+        y4 = x.transpose(1, 2)
+        y4 = y4.view(-1,30,512,1)
+
+        y = torch.cat((y1,y2,y3,y4), dim=3)
+        y = self.maxpool(y)
+        y = y.view(-1,30,512)
+        
+        return y
+
